26f83803e42665825d0c7958d31682a48f5fabe0
Stephen Broadfoot <sbro@Stephens-MacBook-Air.local>
2013-12-07 10:19:01 +1100

    basic user authentication

--- src/wmc.go
+++ src/wmc.go
@@ -3,6 +3,9 @@ package wmc
 import (
 	"fmt"
 	"net/http"
+	
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
@@ -10,5 +13,21 @@ func init() {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello web!")
+	c := appengine.NewContext(r)
+	
+	u := user.Current(c)
+	
+	if u == nil {
+		loginURL, err := user.LoginURL(c, "/")
+		
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		
+		fmt.Fprint(w, "<html><body><a href=" + loginURL + ">login</a></body></html>")
+	} else {
+		fmt.Fprint(w, "Hello, you are logged in as ", u.String())
+	}
+	
 }
