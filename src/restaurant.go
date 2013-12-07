a58e1d723f1828235dd75aa2431caafa662e7dc0
Stephen Broadfoot <stephen.broadfoot@gmail.com>
2013-12-07 22:20:38 +1100

    Added Restaurant type

---
+++ src/restaurant.go
@@ -0,0 +1,52 @@
+package wmc
+
+import (
+	"net/http"
+
+	"appengine"
+	"appengine/datastore"
+)
+
+type Restaurant struct {
+	Name       	string
+	Address    	string
+}
+
+func restaurantHandler(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("rid") == "" {
+		http.Error(w, "Profile Not Found", http.StatusNotFound)
+	}
+	
+	loginInfo := loginDetails(r)
+
+	rest, rid := targetRestaurant(r)
+
+	if rest == nil {
+		http.Error(w, "Restaurant Not Found", http.StatusNotFound)
+		return
+	}
+
+	outputToJsonOrTemplate(w, r, struct {
+		LoginInfo *LoginInfo
+		RID   string
+		Restaurant *Restaurant
+	}{
+		loginInfo,
+		rid,
+		rest,
+	}, "restaurant")
+}
+
+func retrieveRestaurant(c appengine.Context, rid string) *Restaurant {
+	key := datastore.NewKey(c, "Restaurant", rid, 0, nil)
+	var rest Restaurant
+
+	err := datastore.Get(c, key, &rest)
+
+	if err == datastore.ErrNoSuchEntity {
+		return nil
+	}
+	check(err)
+	
+	return &rest
+}
